ff225e1efb00a916c9d15c0eba8edd2d12071f52
PRAFFUL RANJAN <40630844+prafful360@users.noreply.github.com>
2019-01-15 02:40:18 +0530

    Add files via upload

---
+++ databaseurl/dburl.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURL:
+    "mongodb://<databaseuser>:<databasepassword>@ds455699.mlab.com:55964/database",
+  secret: "secretisneccessary" //it can be anything use it for json token
+};
